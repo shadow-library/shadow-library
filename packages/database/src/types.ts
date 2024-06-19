ddd47daf69ef484fd99c0948a4fdc6fdf737a9a5
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-06-19 20:08:08 +0400

    feat(database): collections and formats typescript types (#23)

---
+++ packages/database/src/types.ts
@@ -0,0 +1,106 @@
+/**
+ * Importing npm packages
+ */
+
+/**
+ * Importing user defined packages
+ */
+
+/**
+ * Defining types
+ */
+
+export type NumberDataType = 'int' | 'long' | 'double';
+
+export type StringDataType = 'uniqueId' | 'string' | 'objectId';
+
+export type DataType = StringDataType | NumberDataType | 'boolean' | 'date' | 'object' | 'array';
+
+export interface SchemaField {
+  type: DataType;
+  description?: string;
+  required?: boolean | string;
+  format?: string;
+  validator?: string;
+  transformer?: string;
+  defaultFn?: string;
+}
+
+export interface BooleanSchemaField extends SchemaField {
+  type: 'boolean';
+  default?: boolean;
+}
+
+export interface StringSchemaField extends SchemaField {
+  type: 'string';
+  default?: string;
+  transform?: 'lowercase' | 'uppercase' | 'capitalize';
+  trim?: boolean;
+}
+
+export interface NumberSchemaField extends SchemaField {
+  type: NumberDataType;
+  default?: number;
+  minimum?: number;
+  maximum?: number;
+  enum?: number[];
+}
+
+export interface DateSchemaField extends SchemaField {
+  type: 'date';
+  default?: Date;
+  minimum?: Date;
+  maximum?: Date;
+}
+
+export interface ObjectSchemaField extends SchemaField {
+  type: 'object' | 'array';
+  subType: DataType | Document;
+}
+
+export type MixedDocument = { $type: string; $key: string };
+
+export type FixedDocument = Record<string, SchemaField>;
+
+export type Document = FixedDocument | MixedDocument;
+
+export interface Index {
+  name?: string;
+  keys: Record<string, 1 | -1>;
+  unique?: boolean;
+  expireAfterSeconds?: number;
+}
+
+export interface Relation {
+  collection: string;
+  localField: string;
+  foreignField: string;
+}
+
+export interface Projection extends Record<string, boolean | Projection> {}
+
+export interface Collection {
+  name: string;
+  alias?: string;
+  description?: string;
+  createdAt?: boolean;
+  updatedAt?: boolean;
+  schema: { type: 'uniqueId' | 'objectId'; alias?: string } & Document;
+  indexes?: Index[];
+  relations?: Relation[];
+  projections?: Record<string, Projection>;
+}
+
+export interface Collections {
+  name?: string;
+  description?: string;
+  definitions?: Record<string, Document>;
+  collections: Collection[];
+}
+
+export interface Format {
+  pattern: string;
+  description?: string;
+}
+
+export type Formats = Record<string, Format>;
