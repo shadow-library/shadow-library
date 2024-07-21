1de36135384ea7bae060839486cd1915a423770b
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-07-21 12:52:22 +0400

    feat(app): global module decorator (#60)

---
+++ packages/app/src/decorators/global-module.decorator.ts
@@ -0,0 +1,34 @@
+/**
+ * Importing npm packages
+ */
+import { InternalError } from '@shadow-library/errors';
+
+/**
+ * Importing user defined packages
+ */
+import { Module, ModuleMetadata } from './module.decorator';
+import { GLOBAL_WATERMARK, MODULE_METADATA } from '../constants';
+
+/**
+ * Defining types
+ */
+
+export type GlobalModuleMetadata = Pick<ModuleMetadata, 'providers' | 'exports'>;
+
+/**
+ * Declaring the constants
+ */
+const invalidMetadataKeys = [MODULE_METADATA.CONTROLLERS, MODULE_METADATA.IMPORTS] as string[];
+
+export function GlobalModule(metadata: GlobalModuleMetadata): ClassDecorator {
+  const properties = Object.keys(metadata);
+  for (const property of properties) {
+    const invalid = invalidMetadataKeys.includes(property);
+    if (invalid) throw new InternalError(`Invalid property '${property}' passed into the @GlobalModule() decorator.`);
+  }
+
+  return target => {
+    Reflect.defineMetadata(GLOBAL_WATERMARK, true, target);
+    Module(metadata)(target);
+  };
+}
