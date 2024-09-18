9ea1ebbd8d6898160a5c6c0dde827169f5c13c1b
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-09-18 19:38:08 +0400

    feat(common): throwError shorthand (#121)

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,2 +1,3 @@
 export * from './errors';
 export * from './services';
+export * from './shorthands';
---
+++ packages/common/src/shorthands.ts
@@ -0,0 +1,19 @@
+/**
+ * Importing npm packages
+ */
+
+/**
+ * Importing user defined packages
+ */
+
+/**
+ * Defining types
+ */
+
+/**
+ * Declaring the constants
+ */
+
+export function throwError(error: Error): never {
+  throw error;
+}
