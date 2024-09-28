a3a6ea8d8bc3369e8f9f6dc852f8c7c488e94449
Leander Paul <leanderpaul97@outlook.com>
2024-09-28 22:33:02 +0400

    feat(server): handle validation error

--- packages/server/src/classes/default-error-handler.ts
+++ packages/server/src/classes/default-error-handler.ts
@@ -1,7 +1,7 @@
 /**
  * Importing npm packages
  */
-import { AppError } from '@shadow-library/common';
+import { AppError, ValidationError } from '@shadow-library/common';
 
 /**
  * Importing user defined packages
@@ -20,6 +20,7 @@ import { ServerError } from '../server.error';
 export class DefaultErrorHandler implements ErrorHandler {
   handle(err: Error, _req: HttpRequest, res: HttpResponse): HttpResponse {
     if (err instanceof ServerError) return res.status(err.getStatusCode()).send(err.toObject());
+    else if (err instanceof ValidationError) return res.status(400).send(err.toObject());
     else if (err instanceof AppError) return res.status(500).send(err.toObject());
     return res.status(500).send({ message: err.message ?? 'Internal Server Error' });
   }
