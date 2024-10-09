feeb6adc7670a29461f8fee7ff52e95fa5456250
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-10-09 21:50:27 +0400

    feat(common): try catch shorthand (#149)

--- packages/common/src/shorthands.ts
+++ packages/common/src/shorthands.ts
@@ -10,10 +10,24 @@
  * Defining types
  */
 
+export type TryResult<TError, TResult> = { success: true; data: TResult } | { success: false; error: TError };
+
 /**
  * Declaring the constants
  */
 
 export function throwError(error: Error): never {
   throw error;
+}
+
+export function tryCatch<TError extends Error, TResult>(fn: () => Promise<TResult>): Promise<TryResult<TError, TResult>>;
+export function tryCatch<TError extends Error, TResult>(fn: () => TResult): TryResult<TError, TResult>;
+export function tryCatch<TError extends Error, TResult>(fn: () => TResult | Promise<TResult>): TryResult<TError, TResult> | Promise<TryResult<TError, TResult>> {
+  try {
+    const data = fn();
+    if (data instanceof Promise) return data.then(data => ({ success: true, data })).catch(error => ({ success: false, error })) as Promise<TryResult<TError, TResult>>;
+    return { success: true, data };
+  } catch (error: any) {
+    return { success: false, error };
+  }
 }
