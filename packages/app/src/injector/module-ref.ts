cdfed35184712379c2b53f04306e8cc65a0b169e
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-10-12 13:10:54 +0400

    feat(app): module ref (#152)

---
+++ packages/app/src/injector/module-ref.ts
@@ -0,0 +1,33 @@
+/**
+ * Importing npm packages
+ */
+import { Class } from 'type-fest';
+
+/**
+ * Importing user defined packages
+ */
+import { InjectionToken } from '../interfaces';
+import { ContextId } from '../utils';
+
+/**
+ * Defining types
+ */
+
+/**
+ * Declaring the constants
+ */
+
+export abstract class ModuleRef {
+  /**
+   * Retrieves an instance of a provider otherwise, throws exception.
+   * @returns {TResult}
+   */
+  abstract get<TInput = any, TResult = TInput>(token: Class<TInput>): TResult;
+  abstract get<TInput = any, TResult = TInput>(token: InjectionToken): TResult;
+
+  /**
+   * Resolves transient instance of a provider otherwise, throws exception.
+   * @returns {Array<TResult>}
+   */
+  abstract resolve<TInput = any, TResult = TInput>(typeOrToken: Class<TInput>, contextId: ContextId): Promise<TResult>;
+}
