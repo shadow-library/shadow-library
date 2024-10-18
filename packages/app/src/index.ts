a23c23276a38654cca32949e5e0aa175324d706c
Leander Paul <37008159+leanderpaul@users.noreply.github.com>
2024-10-18 20:47:04 +0400

    chore(app): clean up unneccessary files (#159)

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -11,8 +11,5 @@ export { ModuleRef } from './injector';
 export * from './interfaces';
 export * from './utils';
 
-export { PARAMTYPES_METADATA, RETURN_TYPE_METADATA } from './constants';
 export * from './shadow-application';
 export * from './shadow-factory';
-
-export type { RouteController } from './injector';
--- packages/app/src/injector/helpers/index.ts
+++ packages/app/src/injector/helpers/index.ts
@@ -1,7 +1,3 @@
-export * from './extractor.helper';
-export * from './parser.helper';
-export * from './validator.helper';
-
 export * from './dependency-graph';
 export * from './di-errors';
 export * from './provider-classifier';
