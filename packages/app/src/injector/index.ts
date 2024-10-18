d30a3652255e852b37cdef4fdd02faed669afedc
Leander Paul <leanderpaul97@outlook.com>
2024-10-18 20:45:53 +0400

    chore(app): clean up unneccessary files

--- packages/app/src/injector/index.ts
+++ packages/app/src/injector/index.ts
@@ -1,7 +1,5 @@
-export * from './controller-wrapper';
 export * from './instance-wrapper';
 export * from './module-ref';
 export * from './helpers';
 export * from './module-registry';
-export * from './module-wrapper';
 export * from './module';
--- packages/app/src/interfaces/index.ts
+++ packages/app/src/interfaces/index.ts
@@ -1,5 +1,2 @@
-export * from './application-config.interface';
-export * from './executable.interface';
 export * from './module-lifecycle.interface';
 export * from './provider.interface';
-export * from './router.interface';
